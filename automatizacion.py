3735f6105dc6462e88d483a0dd7e9b38c0519fc7
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-02 17:37:13 -0300

    asd

--- automatizacion.py
+++ automatizacion.py
@@ -3,11 +3,14 @@ import json
 import pandas as pd
 import sys
 from bs4 import BeautifulSoup
+import shutil
+import datetime
 
 #
 
 def proceso():
     print("Hola")
+    shutil.copy (r'Consolidado/Consolidado.xlsx', f"Legacy/Consolidado_{datetime.datetime.now().strftime('%Y-%m-%d')}.xlsx")
     return 
 
 if __name__ == '__main__':
