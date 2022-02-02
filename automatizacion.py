821b5797a31df9fc703ef81aa4163b79588b2b12
lmonsalve22 <lmonsalve22@gmail.com>
2022-02-02 17:18:05 -0300

    asd

---
+++ automatizacion.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import pandas as pd
+import sys
+from bs4 import BeautifulSoup
+
+#
+
+def proceso():
+    print("Hola")
+    return 
+
+if __name__ == '__main__':
+    try:
+        proceso()
+    except:
+        try:
+            proceso()
+        except:
+            error = sys.exc_info()[1]
+            print(error)
+        
+    
